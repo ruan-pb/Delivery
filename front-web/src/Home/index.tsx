e60ef00d995620c163842627a786e888985421b9
Ruan-_pb <ruancruzsoares@hotmail.com>
2021-01-08 15:38:43 -0300

    add Footer

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,34 +1,40 @@
 import './styles.css'
-import {ReactComponent as Logo} from './logo.svg';
-import {ReactComponent as MainImage} from './main.svg';
-function Home(){
-    return(
-        <div className="home-container"> 
-        
-            <div className="home-content">
-                <div className="home-actions">
-                    <h1 className="home-title">
+import { ReactComponent as Logo } from './logo.svg';
+import { ReactComponent as MainImage } from './main.svg';
+import Footer from '../Footer';
+function Home() {
+    return (
+        <>
+            <div className="home-container">
 
-                        Faça seu pedido<br/> que entregamos<br/> pra você!!!
-                        
+                <div className="home-content">
+                    <div className="home-actions">
+                        <h1 className="home-title">
+
+                            Faça seu pedido<br /> que entregamos<br /> pra você!!!
+                            
                     </h1>
-                    <h3 className="home-subtitle">
-                        Escolha o seu pedido e em poucos minutos <br/> levaremos até sua porta<br/>
-                     </h3>
+                        <h3 className="home-subtitle">
+                            Escolha o seu pedido e em poucos minutos <br /> levaremos até sua porta<br />
+                        </h3>
 
                         <a href="ordres" className="home-btt-order">
-                             Fazer Pedido
+                            Fazer Pedido
                          </a>
 
-                </div>
-                <div className="home-image">
-                    <MainImage/>
+                    </div>
+                    <div className="home-image">
+                        <MainImage />
 
+                    </div>
                 </div>
+
+
             </div>
-            
-        
-        </div>
+
+            <Footer/>
+
+        </>
     )
 
 }
